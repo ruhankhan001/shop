ee635f747721ed31a58f269c72753f3a8f4685f9
ruhankhan001 <ruhannn.01khan@gmail.com>
2023-02-24 18:11:05 +0530

    wjkjk

--- completeweb.js
+++ completeweb.js
@@ -1,5 +1,47 @@
-function clickher(){
-    if(!confirm('LETS GO')){
 
-    }
-}
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+
+// }
+// let x=[1,2,3,4,5];
+// for ( val of x) {
+//     console.log(val);
+// }
+// let y=1;
+
+// while (y <= 10) {
+//     console.log(y);
+//     y++;
+// }
+
+// x.forEach(element => {
+//    if (element% 2 == 0) {
+//     console.log(element);
+//    }
+// });
+//  
+// Array loop if else datatype string math function 
+
+// for (let i = 0; i <= 50; i++) {
+//    if (i % 5 == 0) {
+//       console.log(i);
+//    }
+// }
+
+// let Hindi = Number (prompt('What is your hindi number'));
+// let English = Number (prompt('What is your english number'));
+// let maths = Number (prompt('What is your maths number'));
+// let history = Number (prompt('What is your history number'));
+// let science = Number (prompt('What is your science number'));
+
+//  let total = Hindi + English + maths + history + science;
+// let percentage = (total/500)*100;
+
+// if (percentage>100) {
+//    console.log('undefined');
+// } else {
+//    console.log(percentage);
+// }
+
+
